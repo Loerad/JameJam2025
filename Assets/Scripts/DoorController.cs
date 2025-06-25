463355f35be3b17b7f6ccf03da5837fc535c5d0e
Rohan <anakrv1@student.op.ac.nz>
2025-06-25 16:09:08 +1200

    feat: add and connect all doors

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     [SerializeField]
-    private DoorController connectedDoor;
+    private GameObject connectedDoor;
     [SerializeField]
     private int connectedDoorRoomIndex;
     private GameObject player;
     private DoorInteraction interactor;
+    [SerializeField]
+    private bool isGameExit;
+    [SerializeField]
+    private bool isLocked;
 
     void Start()
     {
@@ -17,7 +21,9 @@ public class DoorController : MonoBehaviour
 
     public void OnInteraction()//may need callback
     {
+        player.GetComponent<CharacterController>().enabled = false;
         player.transform.position = connectedDoor.transform.position;
+        player.GetComponent<CharacterController>().enabled = true;
         RoomManager.Instance.ChangeRoom(RoomManager.Instance.GetCurrentRoomFromIndex(connectedDoorRoomIndex));
     }
 
@@ -29,7 +35,7 @@ public class DoorController : MonoBehaviour
             interactor.ReceiveDoor(gameObject);
         }
     }
-    
+
     void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) 
