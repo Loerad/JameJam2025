be5df60ac1e7e82e1a88554af41bf37fb8592d61
Rohan <anakrv1@student.op.ac.nz>
2025-06-25 14:38:29 +1200

    feat: add door behaviour

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -3,21 +3,36 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     [SerializeField]
-    private RoomManager roomManager;
-    [SerializeField]
     private DoorController connectedDoor;
     [SerializeField]
-    private int roomIndex;
+    private int connectedDoorRoomIndex;
     private GameObject player;
+    private DoorInteraction interactor;
 
     void Start()
     {
-        roomManager = GameObject.Find("Main Camera").GetComponent<RoomManager>();
         player = GameObject.Find("Player");
+        interactor = player.GetComponent<DoorInteraction>();
     }
 
     public void OnInteraction()//may need callback
     {
         player.transform.position = connectedDoor.transform.position;
+        RoomManager.Instance.ChangeRoom(RoomManager.Instance.GetCurrentRoomFromIndex(connectedDoorRoomIndex));
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactor.ReceiveDoor(gameObject);
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) 
+        {
+            interactor.ForgetDoor();
+        }
     }
 }
